409ef27c4886785e7ddc6b0ae710089dc279a3d6
Federico Orlandau <federico.orlandau@valtech.com>
2021-07-19 17:38:55 +0200

    fix(ssr): fix ssr files

--- pagetemplates/[[...slug]].ssr.tsx
+++ pagetemplates/[[...slug]].ssr.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from 'next';
 
 import LandingPage, { LandingPageProps } from '../components/LandingPage/LandingPage';
-import { getPageBySlug, getEntryByUid } from '../lib/api';
+import { getPageBySlug } from '../lib/api';
 
 export default LandingPage;
 
@@ -14,13 +14,10 @@ export const getServerSideProps: GetServerSideProps<LandingPageProps> = async (c
 
   const page = await getPageBySlug(context.preview || false, slug);
 
-  const components = await Promise.all(page?.components?.map((component) => getEntryByUid(component.uid, component._content_type_uid)) ?? []);
-
   return {
     props: {
       slug,
       page,
-      components,
     },
   };
 };

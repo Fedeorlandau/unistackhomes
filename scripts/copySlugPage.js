e6ceccbabe7fe3c5f074637b494b0fbf1053795c
Federico Orlandau <federico.orlandau@gmail.com>
2021-08-10 17:17:19 +0200

    fix(nextjs): enable SSR

--- scripts/copySlugPage.js
+++ scripts/copySlugPage.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 require('dotenv').config();
 
-console.log("Copying templates")
-
-fs.copyFileSync('./pagetemplates/[[...slug]].ssr.tsx', './pages/[[...slug]].tsx');
+if (process.env.NEXT_USE_SSR === '1') {
+  fs.copyFileSync('./pagetemplates/[[...slug]].ssr.tsx', './pages/[[...slug]].tsx');
+} else {
+  fs.copyFileSync('./pagetemplates/[[...slug]].ssg.tsx', './pages/[[...slug]].tsx');
+}
